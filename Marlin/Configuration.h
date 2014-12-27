64adae423ec4d4b7a6d259762f8553fa8ce82213
Jotham <jonnyboysmithy@gmail.com>
2014-12-28 11:36:49 +1300

    clarified comment on acceleration and jerk settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -591,9 +591,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {XStepsPulley, YStepsPulley, ZStepsPulley, 610.2}
 
 // delta speeds must be the same on xyz
+// divide these values by approximately 2.5 as marlin calculates the speed at the motors, not the end effector
 #define DEFAULT_MAX_FEEDRATE          {50, 50, 50, 40}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {400,400,400,3000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
+// divide these values by approximately 2.5 as marlin calculates the speed at the motors, not the end effector
 #define DEFAULT_ACCELERATION          400    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
 
@@ -604,6 +606,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // #define EXTRUDER_OFFSET_Y {0.0, 5.00}  // (in mm) for each extruder, offset of the hotend on the Y axis
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
+// divide these values by approximately 2.5 as marlin calculates the speed at the motors, not the end effector
 #define DEFAULT_XYJERK                20.0    // (mm/sec)
 #define DEFAULT_ZJERK       DEFAULT_XYJERK    // same as xY jerk for delta
 #define DEFAULT_EJERK                 5.0    // (mm/sec)
